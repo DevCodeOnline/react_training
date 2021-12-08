b428f09feec6da0132d23d5a334b8241c87fefbd
GlebKnyzev <grafkrestovsky@mail.ru>
2021-12-08 19:44:44 +0300

    Test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,7 @@ import s from './Header.module.css';
 import {NavLink} from "react-router-dom";
 
 const Header = (props) => {
-	return <header className={s.header}>
-		<img src="https://i.pinimg.com/originals/57/2e/4f/572e4f51d0a4d67669784df53026b5a7.png" />
-		<div className={s.loginBlock}>
-			{ props.isAuth ? 'Логин: ' + props.login :
-				<NavLink to={'/login'}>Login</NavLink>
-			}
-		</div>
-	</header>
+	return <div>1</div>
 };
 
 export default Header;

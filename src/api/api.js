a335200ae199c92ad873ab8867eb83d9b4dc199f
GlebKnyzev <grafkrestovsky@mail.ru>
2021-12-12 20:40:45 +0300

    Work - 63 - real

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": "9081f0cb-85ce-4ecb-bfa5-44c418a52097"
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => {
+            return response.data
+        })
+    }
+}
